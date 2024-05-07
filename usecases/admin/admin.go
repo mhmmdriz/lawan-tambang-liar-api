bd404f69122cc8320a53157dd94852edcc465e4c
mhmmdriz <rizki25030@gmail.com>
2024-05-07 18:41:18 +0700

    Memperbaiki response di usecase admin

--- usecases/admin/admin.go
+++ usecases/admin/admin.go
@@ -1,7 +1,6 @@
 package admin
 
 import (
-	"errors"
 	"lawan-tambang-liar/constants"
 	"lawan-tambang-liar/entities"
 	"lawan-tambang-liar/middlewares"
@@ -20,30 +19,10 @@ func NewAdminUseCase(repository entities.AdminRepositoryInterface) *AdminUseCase
 
 func (u *AdminUseCase) CreateAccount(admin *entities.Admin) (entities.Admin, error) {
 	if admin.Email == "" || admin.Password == "" || admin.Username == "" || admin.RegencyID == "" || admin.DistrictID == "" || admin.Address == "" {
-		return entities.Admin{}, errors.New("all fields must be filled")
+		return entities.Admin{}, constants.ErrAllFieldsMustBeFilled
 	}
 
 	err := u.repository.CreateAccount(admin)
-
-	if err != nil {
-		return entities.Admin{}, err
-	}
-
-	return *admin, nil
-}
-
-func (u *AdminUseCase) Login(admin *entities.Admin) (entities.Admin, error) {
-	if admin.Username == "" || admin.Password == "" {
-		return entities.Admin{}, errors.New("all fields must be filled")
-	}
-
-	err := u.repository.Login(admin)
-
-	if admin.IsSuperAdmin {
-		(*admin).Token = middlewares.GenerateTokenJWT(admin.ID, admin.Username, "super_admin")
-	} else {
-		(*admin).Token = middlewares.GenerateTokenJWT(admin.ID, admin.Username, "admin")
-	}
 
 	if err != nil {
 		if strings.HasPrefix(err.Error(), "Error 1062") {
@@ -55,6 +34,25 @@ func (u *AdminUseCase) Login(admin *entities.Admin) (entities.Admin, error) {
 		} else {
 			return entities.Admin{}, constants.ErrInternalServerError
 		}
+	}
+
+	return *admin, nil
+}
+
+func (u *AdminUseCase) Login(admin *entities.Admin) (entities.Admin, error) {
+	if admin.Username == "" || admin.Password == "" {
+		return entities.Admin{}, constants.ErrAllFieldsMustBeFilled
+	}
+
+	err := u.repository.Login(admin)
+	if err != nil {
+		return entities.Admin{}, constants.ErrInvalidUsernameOrPassword
+	}
+
+	if admin.IsSuperAdmin {
+		(*admin).Token = middlewares.GenerateTokenJWT(admin.ID, admin.Username, "super_admin")
+	} else {
+		(*admin).Token = middlewares.GenerateTokenJWT(admin.ID, admin.Username, "admin")
 	}
 
 	return *admin, nil
